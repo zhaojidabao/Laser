814eb8ec2c5a6ec16c52e5291849777eba420fdf
zhaojidabao <42225836+zhaojidabao@users.noreply.github.com>
2018-08-31 09:38:48 +0800

    Revert "1"
    This reverts commit cd8713451f4c3f2df3da45f671d584973f1feb7e.

--- HL6.0.4_VS2008DB/HL404/create.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set msdevdir=D:\Windows\Microsoft Visual Studio\Common\MSDev98
-set VcOsDir=WIN95
-if "%OS%" == "Windows_NT" set VcOsDir=WINNT
-if "%OS%" == "Windows_NT" set PATH=%MSDevDir%\BIN;%MSVCDir%\BIN;%VSCommonDir%\TOOLS\%VcOsDir%;%VSCommonDir%\TOOLS;%PATH%
-if "%OS%" == "" set PATH="%MSDevDir%\BIN";"%MSVCDir%\BIN";"%VSCommonDir%\TOOLS\%VcOsDir%";"%VSCommonDir%\TOOLS";"%windir%\SYSTEM";"%PATH%"
-
-MSDEV .\RCFactory.dsw /make "rcfactory - win32 release" /REBUILD /OUT .\RC.TXT
--- HL6.0.4_VS2008DB/code/CmdBroadenStroke.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CmdBaseSave.h"
-
-class CCmdBroadenStroke : public CCmdBaseSave
-{
-public:
-	CCmdBroadenStroke(CHLDoc *pDoc);
-	~CCmdBroadenStroke(void);
-};
